2bec66d5a3a8338e0d0749aba5565292e2cac8bd
Singh-Virat <76221562+Singh-Virat@users.noreply.github.com>
2022-09-08 18:24:26 +0530

    Version 2.3

---
+++ dashboard.js
@@ -0,0 +1,23 @@
+var mySidebar = document.getElementById("mySidebar");
+
+var overlayBg = document.getElementById("myOverlay");
+
+function w3_close() 
+{
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+
+function w3_open()
+{
+  if(mySidebar.style.display === 'block') 
+  {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  }
+  else
+  {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
